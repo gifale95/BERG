36dfb94f0301b735edfb81d489e74e9223b05fe1
Domenic <67119745+ToastyDom@users.noreply.github.com>
2026-02-17 18:40:52 +0100

    Typo

--- berg/models/fmri/text2fmri_utils/feature_extractor.py
+++ berg/models/fmri/text2fmri_utils/feature_extractor.py
@@ -53,7 +53,7 @@ class FeatureExtractor:
                 self.berg_dir,
                 "encoding_models",
                 "modality-fmri",
-                "train_dataset-cneuromo_algo2025",
+                "train_dataset-cneuromod_algo2025",
                 "model-text2fmri",
                 "encoding_models_weights"
             )
