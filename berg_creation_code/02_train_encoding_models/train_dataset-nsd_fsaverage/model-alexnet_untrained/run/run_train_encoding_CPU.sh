1d831ac6066e15cc2f81f7f26b8e9e30c5d0fa42
gifale95 <alessandro.gifford@gmail.com>
2026-02-28 23:33:31 +0100

    edit

--- berg_creation_code/02_train_encoding_models/train_dataset-nsd_fsaverage/model-alexnet_untrained/run/run_train_encoding_CPU.sh
+++ berg_creation_code/02_train_encoding_models/train_dataset-nsd_fsaverage/model-alexnet_untrained/run/run_train_encoding_CPU.sh
@@ -2,8 +2,8 @@
 #SBATCH --mail-user=giffordale95@zedat.fu-berlin.de
 #SBATCH --job-name=berg-nsd_fsaverage_alexnet_untrained-train_encoding_CPU
 #SBATCH --mail-type=end
-#SBATCH --mem=40000
-#SBATCH --time=04:00:00
+#SBATCH --mem=50000
+#SBATCH --time=10:00:00
 #SBATCH --qos=extended
 
 # Create the parameters combinations
