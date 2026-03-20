de9e66b36ff4369f92c82fe45741f18e9f72dcee
Alessandro Gifford <alessandro.gifford@gmail.com>
2026-03-20 11:07:45 +0100

    Simplify random seed assignment
    Removed conditional random seed assignment based on monkey type.

--- berg_creation_code/02_train_encoding_models/train_dataset-tvsd/vit_b_32/train_encoding.py
+++ berg_creation_code/02_train_encoding_models/train_dataset-tvsd/vit_b_32/train_encoding.py
@@ -76,10 +76,6 @@ for key, val in vars(args).items():
     print('{:16} {}'.format(key, val))
 
 # Set random seed for reproducible results
-if args.monkey == 'monkeyN':
-   seed = 20200220
-elif args.monkey == 'monkeyF':
-   seed = 20200221
 np.random.seed(seed)
 torch.manual_seed(seed)
 
