d825333176822960bceaa27ab7aa647f4c199ca0
gifale95 <alessandro.gifford@gmail.com>
2026-03-24 10:38:09 +0100

    Remove slurm files.

--- berg_creation_code/03_test_encoding_models/train_dataset-things_eeg_2/02_plot.py
+++ berg_creation_code/03_test_encoding_models/train_dataset-things_eeg_2/02_plot.py
@@ -28,7 +28,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--subjects', type=list, default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 parser.add_argument('--channels', type=str, default='O') # ['O', 'P', 'T', 'C', 'F', 'all']
 parser.add_argument('--model', type=str, default='vit_b_32')
-parser.add_argument('--berg_dir', default='/scratch/giffordale95/projects/brain-encoding-response-generator', type=str) # !!!
+parser.add_argument('--berg_dir', default='../brain-encoding-response-generator', type=str)
 args, unknown = parser.parse_known_args()
 
 
