43db35178e37ec52ed5d888ad9e4d140118bde65
Domenic <67119745+ToastyDom@users.noreply.github.com>
2026-04-09 11:34:50 +0200

    Update __init__.py

--- berg/models/fmri/__init__.py
+++ berg/models/fmri/__init__.py
@@ -12,7 +12,7 @@ import berg.models.fmri.tuckute_2024_gpt2_xl
 import berg.models.fmri.mosaic_CNN8_multihead_subNSD_verticesAll
 import berg.models.fmri.mosaic_CNN8_multihead_subAll_verticesVisual
 import berg.models.fmri.cneuromod_algo2025_text2fmri
-import berg.models.fmri.eng1000_ridge
+import berg.models.fmri.opt_1_3b_ridge
 try:
     import berg.models.fmri.brainscore_language_models
 except ImportError:
