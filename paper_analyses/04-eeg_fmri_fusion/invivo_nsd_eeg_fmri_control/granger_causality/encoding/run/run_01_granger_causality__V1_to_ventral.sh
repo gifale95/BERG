c9fd6eefb69e453d6de1fb0fd33e018bef08edfe
gifale95 <alessandro.gifford@gmail.com>
2026-05-14 13:52:03 +0200

    edit

--- paper_analyses/04-eeg_fmri_fusion/invivo_nsd_eeg_fmri_control/granger_causality/encoding/run/run_01_granger_causality__V1_to_ventral.sh
+++ paper_analyses/04-eeg_fmri_fusion/invivo_nsd_eeg_fmri_control/granger_causality/encoding/run/run_01_granger_causality__V1_to_ventral.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --mail-user=giffordale95@zedat.fu-berlin.de
-#SBATCH --job-name=eeg_fmri_fusion-invivo_nsd_eeg_fmri_control-granger_causality-encoding-01_granger_causality__V1_to_hV4__DELETE_1
+#SBATCH --job-name=eeg_fmri_fusion-invivo_nsd_eeg_fmri_control-granger_causality-encoding-01_granger_causality__V1_to_ventral
 #SBATCH --mail-type=end
 #SBATCH --mem=25000
 #SBATCH --time=60:00:00
@@ -14,12 +14,12 @@ declare -a eeg_train_trials_all
 declare -a regression_all
 declare -a time_split_all
 index=0
-for fs in '1' ; do
+for fs in '1' '4' '5' '6' '7' '8' ; do
     for rs in 'V1' ; do
-        for rt in 'hV4' ; do
+        for rt in 'ventral' ; do
             for tr in 'even' 'odd' ; do
-                for r in 'ridge' ; do
-                    for t in `seq 0 9` ; do
+                for r in 'linear' 'ridge' ; do
+                    for t in `seq 0 9` ; do # !!! `seq 0 9`
                         subject_all[$index]=$fs
                         roi_source_all[$index]=$rs
                         roi_target_all[$index]=$rt
